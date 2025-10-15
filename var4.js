4d1ea26744431adad877191459aebc5d27a1c3d9
ZahidMemon <zmemon106@gmail.com>
2025-10-15 16:15:00 +0530

    if else done

---
+++ var4.js
@@ -0,0 +1,12 @@
+// Operators
+/* Now we are 
+learning Operators */
+
+let age = 18;
+let text = "You can drive";
+
+if (age <= 18) {
+    text = "You cannot drive"
+}
+
+document.getElementById("demo").innerHTML=text;
---
+++ var5.js
@@ -0,0 +1,8 @@
+let a = 4;
+
+if (a%2 == 0){
+    a = "Even";
+}
+else {a = "odd"}
+
+document.getElementById("demo2").innerHTML=a;
