3136549dae411d715c7a8853d32ceccdd5d83f5f
ZahidMemon <zmemon106@gmail.com>
2025-10-15 15:48:01 +0530

    data types

---
+++ var3.js
@@ -0,0 +1,69 @@
+//Types of variables
+
+// String
+
+let x = "Hello";
+
+console.log(x)
+console.log(typeof x)
+
+// Number
+
+let y = 100;
+let z = 1.25;
+
+console.log(y,z)
+console.log(typeof y,z)
+
+// Big Int
+
+let s = 25000000000n;
+let a = BigInt(10700000000000);
+
+console.log(s,a)
+console.log(typeof s,a)
+
+// boolean
+
+let m = true;
+let i = false;
+
+console.log(m,i)
+console.log(typeof m)
+console.log(typeof i)
+
+// Object
+
+const var1 = {Name:"Zahid",Surname:"Memon"};
+console.log(var1)
+console.log(typeof var1)
+
+// Array object
+
+const cars = ['BMW','Ciaz','Audi'];
+console.log(cars)
+console.log(typeof cars)
+
+// Date object
+
+const date = new Date(2025-15-1);
+console.log(date)
+console.log(typeof date)
+
+// undefined
+
+let p;
+console.log(p)
+console.log(typeof p)
+
+// Null
+
+let r = null;
+console.log(r)
+console.log(typeof r)
+
+// Symbol
+
+const b = Symbol();
+console.log(b)
+console.log(typeof b)
